c7ad39c8633968b734e528472e2943f5c35c21ec
Mylesofcode <jred_5@yahoo.com>
2025-05-03 13:53:01 -0400

    Add files via upload

---
+++ CSC 500 wk7 assign.py
@@ -0,0 +1,42 @@
+def main():
+    # Dictionary for room numbers
+    room_numbers = {
+        'CSC101': '3004',
+        'CSC102': '4501',
+        'CSC103': '6755',
+        'NET110': '1244',
+        'COM241': '1411'
+    }
+    
+    # Dictionary for instructors
+    instructors = {
+        'CSC101': 'Haynes',
+        'CSC102': 'Alvarado',
+        'CSC103': 'Rich',
+        'NET110': 'Burke',
+        'COM241': 'Lee'
+    }
+    
+    # Dictionary for meeting times
+    meeting_times = {
+        'CSC101': '8:00 a.m.',
+        'CSC102': '9:00 a.m.',
+        'CSC103': '10:00 a.m.',
+        'NET110': '11:00 a.m.',
+        'COM241': '1:00 p.m.'
+    }
+    
+    # Get course number from user
+    course_number = input('Enter a course number: ')
+    
+    # Display course information if the course exists
+    if course_number in room_numbers:
+        print(f'Room Number: {room_numbers[course_number]}')
+        print(f'Instructor: {instructors[course_number]}')
+        print(f'Meeting Time: {meeting_times[course_number]}')
+    else:
+        print(f'Course {course_number} was not found.')
+
+# Call the main function
+if __name__ == '__main__':
+    main()
